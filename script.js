e39ed19dc69a1689df96f2ead5b44620e4e6e97b
AnonymousManan <mananaggarwal2001@gmail.com>
2025-01-21 11:23:21 +0530

    play functionality updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("This is the class for doing the work....")
-
+let currentSong = new Audio()
 async function getSongs() {
     let a = await fetch("/songs");
     let finalresult = await a.text(); // the songs folder can be fetched and converted into the text only.
@@ -16,9 +16,28 @@ async function getSongs() {
     return songs
 }
 
+function formatSecondsToMinutes(seconds) {
+    if (isNaN(seconds)) {
+        return "00:00";
+    }
+    const minutes = Math.floor(seconds / 60); // Get the number of full minutes
+    const remainingSeconds = Math.floor(seconds % 60); // Get the remaining seconds
+    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
+
+const playMusic = (track, pause = false) => {
+    currentSong.src = "/songs/" + track
+    if (!pause) {
+        currentSong.play()
+        play.src = "/img/pause.svg"
+    }
+    document.querySelector(".songtitle").innerHTML = decodeURI(track);
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+}
 async function main() {
     // fetch all the songs from the songs file.
     let songs = await getSongs()
+    playMusic(songs[0], true)
 
     // show all the songs in the library as in the form of list.
     let songsUL = document.querySelector(".songslist").getElementsByTagName("ul")[0]
@@ -26,7 +45,7 @@ async function main() {
         songsUL.innerHTML = songsUL.innerHTML + ` <li>
                             <img class="invert" src="img/music.svg" alt="">
                             <div class="info">
-                                <div>${song.replaceAll("%20", " ").split("320")[0]}</div>
+                                <div>${song.replaceAll("%20", " ")}</div>
                                 <div>Manan</div>
                             </div>
                             <div class="playNow">
@@ -35,11 +54,30 @@ async function main() {
                             </div>
                         </li>`
     }
-    // play the first songs
-    let audioSong = new Audio(songs[1])
-    // audioSong.play()
-    audioSong.addEventListener("timeupdate", (event) => {
-        console.log(event.timeStamp)
+    // Attach an event Listener to each song
+    Array.from(document.querySelector(".songslist").getElementsByTagName("li")).forEach(element => {
+        element.addEventListener("click", (result) => {
+            playMusic(element.querySelector(".info").firstElementChild.innerHTML)
+        })
+    })
+
+    // Attach the event listener to the play, next and previous
+    // paused is not the function it is a variable
+    // we can use id's directly without targeting the element.
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play()
+            play.src = "/img/pause.svg"
+        } else {
+            currentSong.pause()
+            play.src = "/img/play.svg"
+        }
+    })
+    // Listen for the timeupdate event and this event will be running when the song is running.
+    currentSong.addEventListener("timeupdate", (e) => {
+        console.log(currentSong.duration, currentSong.currentTime)
+        document.querySelector(".songtime").innerHTML = `${formatSecondsToMinutes(currentSong.currentTime)}/${formatSecondsToMinutes(currentSong.duration)}`
+        document.querySelector(".circle").style.left = currentSong.currentTime / currentSong.duration * 100 + "%"
     })
 }
 main()
